165b92e93be22478c2a87704260e4f1b1bbfa7db
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-15 19:54:54 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Фоновое изображение</title>
+ </head>
+ <body bgcolor=black background="https://images.app.goo.gl/s2NDWpS5LE7vxVvH8">
+	<p>Здесь фоновое изображение устанавливается с помощью атрибута background тега body.</p>
+
+<h1>PORTFOLIO</h1>
+<hr>
+<h2>Personal data</h2>
+
+<img src="https://images.app.goo.gl/h3sUeZ6e6sQmv5tV9" alt="portfolio image">
+
+</body>
+</html>
+
+
