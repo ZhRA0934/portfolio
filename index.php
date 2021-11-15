c3d0689fbebe82d916a2d2e7b524b5bc16f5742c
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-15 20:01:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 <title>Фоновое изображение</title>
  </head>
- <body bgcolor=Grey background="https://images.app.goo.gl/s2NDWpS5LE7vxVvH8">
+ <body bgcolor=Grey background="images.app.goo.gl/s2NDWpS5LE7vxVvH8">
 	<p>Здесь фоновое изображение устанавливается с помощью атрибута background тега body.</p>
 
 <h1>PORTFOLIO</h1>
 <hr>
 <h2>Personal data</h2>
 
-<img src="https://images.app.goo.gl/h3sUeZ6e6sQmv5tV9" alt="portfolio image">
+<img src="images.app.goo.gl/h3sUeZ6e6sQmv5tV9" alt="portfolio image">
 
 </body>
 </html>
