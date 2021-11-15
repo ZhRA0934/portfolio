340ab25db2b37aab801b81ac81bac4177ad088df
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-15 20:15:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 <title>Фоновое изображение</title>
  </head>
  <body bgcolor=white background="https://images.app.goo.gl/s2NDWpS5LE7vxVvH8">
-	<p>Здесь фоновое изображение устанавливается с помощью атрибута background тега body.</p>
 
 <h1>PORTFOLIO</h1>
 <hr>
 <h2>Personal data</h2>
 
-<img src="https://images.app.goo.gl/NjMGjmN9MDoTGotM7" alt="portfolio image">
+<img src="https://static.wikia.nocookie.net/solo-leveling/images/d/d1/Sungo-%C4%9Cin-vuo.png/revision/latest?cb=20200515042731&path-prefix=ru" alt="portfolio image">
 
 </body>
 </html>
