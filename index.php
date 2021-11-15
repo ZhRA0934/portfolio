8cda063eb57577d713c40cf1ae0ff78f85868712
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-15 20:17:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
 <title>Фоновое изображение</title>
  </head>
- <body bgcolor=white background="https://images.app.goo.gl/s2NDWpS5LE7vxVvH8">
+ <body bgcolor=white background="https://storge.pic2.me/c/1360x800/969/5c88d77aae74c.jpg">
 
 <h1>PORTFOLIO</h1>
+
 <hr>
+
 <h2>Personal data</h2>
 
 <img src="https://static.wikia.nocookie.net/solo-leveling/images/d/d1/Sungo-%C4%9Cin-vuo.png/revision/latest?cb=20200515042731&path-prefix=ru" alt="portfolio image">
