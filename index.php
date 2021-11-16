9b23a8ce56eb2a1d83ede5e4fb4fd5fe800bdb25
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-16 16:07:49 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
 <title>Фоновое изображение</title>
 
  </head>
- <body bgcolor=white background="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzER0hbcawNiuRtjRCU9Nu09NHIMcM0l8r0A&usqp=CAU">
+ <style>
+body{
+background="https://funart.pro/uploads/posts/2021-04/1618601645_38-funart_pro-p-oboi-fon-fon-v-stile-anime-39.jpg">
+moz-background-size: 100%;
+    -webkit-background-size: 100%;
+    -o-background-size: 100%;
+    background-size: 100%;
+   }
+<style>
 
 <h1>PORTFOLIO</h1>
 
@@ -12,7 +20,9 @@
 
 <h2>Personal data</h2>
 
-<p><img src="https://www.meme-arsenal.com/memes/a13202136f3ed3b2ea0135fc70d4d239.jpg" alt="portfolio image" width="220" height="200"></p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" align="left" width="220" height="220" />
+
+
 
 <hr>
 
