01f1e35a4dec489fdbd172558c96aae6fe155c2e
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-16 16:40:37 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Фоновое изображение</title>
-
- </head>
- <style>
-body{
-background="https://funart.pro/uploads/posts/2021-04/1618601645_38-funart_pro-p-oboi-fon-fon-v-stile-anime-39.jpg"
+<style>
+   body{
+   background="https://funart.pro/uploads/posts/2021-04/1618601645_38-funart_pro-p-oboi-fon-fon-v-stile-anime-39.jpg"
    }
 <style>
 
 <style>
    .parent {
-  padding: 10px;
+   padding: 10px;
 }
 img {
   position: relative;
@@ -22,20 +19,16 @@ img {
   width: 20%;
 }
    .round {
-    border-radius: 10000px; /* Радиус скругления */
-    border: px solid green; /* Параметры рамки */
-    box-shadow: 0 0 7px #666; /* Параметры тени */
-   
-  </style>
-
+    border-radius: 10000px;
+    border: px solid green;
+    box-shadow: 0 0 7px #666;
+   </style>
 
 <h1>PORTFOLIO</h1>
-
 <hr>
-
 <h2>Personal data</h2>
 
-<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" alt="fgjffg" align="right" width="220" height="200" class="round"></p>
+<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" alt="fgjffg" align="right" width="410" height="400" class="round"></p>
 
 <p>Name:___________Rustambek.   </p>
 <hr>
@@ -48,8 +41,6 @@ img {
 <p>Phone:__________+99670433439.</p>
 <hr>
 <p>Adress:_________Sokuluk,_Novopavlovka</p>
-
-
 </body>
 </html>
 
