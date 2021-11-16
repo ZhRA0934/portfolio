ed023508a41daef8498572b22639be0d0e611fbb
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-16 16:50:48 +0600

    Update index.php

--- index.php
+++ index.php
@@ -24,11 +24,18 @@ img {
     box-shadow: 0 0 7px #666;
    </style>
 
+<style>
+   .rightpic {
+    float: right; /* Выравнивание по правому краю */
+    margin: 0 0 5px 5px; /* Отступы вокруг фотографии */
+   }
+  </style>
+
 <h1>PORTFOLIO</h1>
 <hr>
 <h2>Personal data</h2>
 
-<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" alt="fgjffg" align="right" width="410" height="400" class="round"></p>
+<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" alt="fgjffg" align="right" width="410" height="400" class="round" class="rightpic"></p>
 
 <p>Name:___________Rustambek.   </p>
 <hr>
