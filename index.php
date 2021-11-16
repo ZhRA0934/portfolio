fd38a76e3c473b4bf78d371052e39343a33f04f7
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-16 16:18:14 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,28 +4,44 @@
 <title>Фоновое изображение</title>
 
  </head>
- 
+ <style>
+body{
+background="https://funart.pro/uploads/posts/2021-04/1618601645_38-funart_pro-p-oboi-fon-fon-v-stile-anime-39.jpg">
+moz-background-size: 100%;
+    -webkit-background-size: 100%;
+    -o-background-size: 100%;
+    background-size: 100%;
+   }
+<style>
+
+
 <h1>PORTFOLIO</h1>
 
 <hr>
 
 <h2>Personal data</h2>
 
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" align="left" width="220" height="220" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+img {
+  border-radius: 50%;
+}
+</style>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU" align="right" width="255" height="250" />
 
 
 
 <hr>
 
-<p>Name:___________Rustambek</p>
+<p>Name:___________Rustambek.   </p>
 <hr>
-<p>Surname:________Zhanabilov</p>
+<p>Surname:________Zhanabilov.  </p>
 <hr>
-<p>Birthday:_______09.03.2004</p>
+<p>Birthday:_______09.03.2004.  </p>
 <hr>
-<p>Class:__________Csc-20a</p>
+<p>Class:__________Csc-20a.     </p>
 <hr>
-<p>Phone:__________+99670433439</p>
+<p>Phone:__________+99670433439.</p>
 <hr>
 <p>Adress:_________Sokuluk,_Novopavlovka</p>
 
